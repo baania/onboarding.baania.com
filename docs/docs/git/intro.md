ba77c221f74a0399c03ac17e6c947b78f8b25bee
Karn Wong <karn.wong+github@mailbox.org>
2022-08-06 11:54:10 +0700

    docs: add git intro

---
+++ docs/docs/git/intro.md
@@ -0,0 +1,9 @@
+---
+title: Intro
+---
+
+- Dev team uses git-flow
+- Data team uses trunk-based development
+- Infra team always merge to master
+
+To create a new repo, use a template from <https://github.com/baania/repo-template> and adjust config accordingly. For team members, go through your team lead to request a new repo.
--- sidebars.js
+++ sidebars.js
@@ -1,72 +1,55 @@
 module.exports = {
-    docs: [
-        "docs/about",
-        "docs/onboarding-plan",
-        {
-            type: "category",
-            label: "First-day",
-            collapsed: false,
-            items: [
-                "docs/first-day/first-day-aml",
-                "docs/first-day/first-day-dev",
-            ],
-        },
-        {
-            type: "category",
-            label: "Git",
-            collapsed: false,
-            items: [
-                "docs/git/git",
-                "docs/git/git-flow",
-                "docs/git/git-trunk",
-            ],
-        },
-        {
-            type: "category",
-            label: "Platform",
-            collapsed: false,
-            items: [
-                "docs/platform/sops",
-                "docs/platform/pre-commit",
-                "docs/platform/pyenv-pipenv",
-                "docs/platform/docker",
-                "docs/platform/kubernetes",
-            ],
-        },
-        {
-            type: "category",
-            label: "Data science",
-            collapsed: false,
-            items: [
-                "docs/data-science/gis",
-                "docs/data-science/postgis",
-                "docs/data-science/postgres",
-                "docs/data-science/pyspark",
-            ],
-        },
-        {
-            type: "category",
-            label: "Misc",
-            collapsed: false,
-            items: [
-                "docs/misc/aws",
-                "docs/misc/terraform",
-                "docs/misc/unix",
-            ],
-        },
-    ],
-    // references: [
+  docs: [
+    "docs/about",
+    "docs/onboarding-plan",
+    {
+      type: "category",
+      label: "First-day",
+      collapsed: false,
+      items: ["docs/first-day/first-day-aml", "docs/first-day/first-day-dev"],
+    },
+    {
+      type: "category",
+      label: "Git",
+      collapsed: false,
+      items: [
+        "docs/git/intro",
+        "docs/git/git",
+        "docs/git/git-flow",
+        "docs/git/git-trunk",
+      ],
+    },
+    {
+      type: "category",
+      label: "Platform",
+      collapsed: false,
+      items: [
+        "docs/platform/sops",
+        "docs/platform/pre-commit",
+        "docs/platform/pyenv-pipenv",
+        "docs/platform/docker",
+        "docs/platform/kubernetes",
+      ],
+    },
+    {
+      type: "category",
+      label: "Data science",
+      collapsed: false,
+      items: [
+        "docs/data-science/gis",
+        "docs/data-science/postgis",
+        "docs/data-science/postgres",
+        "docs/data-science/pyspark",
+      ],
+    },
+    {
+      type: "category",
+      label: "Misc",
+      collapsed: false,
+      items: ["docs/misc/aws", "docs/misc/terraform", "docs/misc/unix"],
+    },
+  ],
+  // references: [
 
-
-
-
-
-
-
-
-
-
-    // ]
-
-
+  // ]
 };
